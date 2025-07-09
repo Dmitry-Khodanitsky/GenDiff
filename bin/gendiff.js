f2874140eec63de621308a31d7cfaff2c0757a3a
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-09 16:16:22 +0300

    удалена кастомная опция вызова help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,5 @@ program
   .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .helpOption('-h, --help [type]', 'display help for command')
   .option('-f, --format', 'output format')
-
 program.parse()
