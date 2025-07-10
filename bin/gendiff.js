979ba7bb7842cc7279c76d79f4061cce72f5dc64
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-10 16:45:33 +0300

    добавлен возможность передать аргументы и добавлен скрипт action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
+import fs from 'fs'
+import path from 'path'
+import { parseData, getDiff } from '../index.js'
+
 const program = new Command()
 
 program
@@ -9,4 +13,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format', 'output format')
+  .action((filepath1, filepath2) => {
+    const fullPathFile1 = path.resolve(process.cwd(), filepath1)
+    const fullPathFile2 = path.resolve(process.cwd(), filepath2)
+    const parsedFile1 = parseData(fullPathFile1)
+    const parsedFile2 = parseData(fullPathFile2)
+    return getDiff(parsedFile1, parsedFile2)
+  })
+// добавить возможность передавать аргументы и вызывать тут функцию получения абсолютного пути
 program.parse()
