1df77d25f532a335d29661041e126039bdf981bc
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-11 17:42:53 +0300

    написана функция нахождения различий getDiff и функция форматирования текста formatText

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import _ from 'lodash'
 
 function parseData(filepath) {
   try {
@@ -11,13 +12,57 @@ function parseData(filepath) {
       default:
         throw new Error('Не подходящий формат файла')
     }
-  } catch (error) {
+  }
+  catch (error) {
     throw new Error(`Возникла ошибка в процессе парсинга: ${error.message}`)
   }
 }
 
 function getDiff(parsedFile1, parsedFile2) {
-  console.log(`Файл 1: ${parsedFile1}, Файл 2: ${parsedFile2}`)
+  // собираем ключи объектов в один массив:
+  const keys1 = Object.keys(parsedFile1)
+  const keys2 = Object.keys(parsedFile2)
+  const keys = _.sortBy(_.union(keys1, keys2))
+
+  const result = {}
+
+  for (const key of keys) {
+    if (!Object.hasOwn(parsedFile1, key)) {
+      result['+ ' + key] = parsedFile2[key]
+    }
+    else if (!Object.hasOwn(parsedFile2, key)) {
+      result['- ' + key] = parsedFile1[key]
+    }
+    else if (parsedFile2[key] !== parsedFile1[key]) {
+      result['- ' + key] = parsedFile1[key]
+      result['+ ' + key] = parsedFile2[key]
+    }
+    else {
+      result['= ' + key] = parsedFile1[key]
+    }
+  }
+  return formatText(JSON.stringify(result))
 }
 
-export { parseData, getDiff }
+function formatText(string) {
+  const result = string.replace(/[{},:"=]/g, (match) => {
+    switch (match) {
+      case '{':
+        return '{ \n'
+      case '}':
+        return '\n}'
+      case ',':
+        return '\n'
+      case ':':
+        return ': '
+      case '"':
+        return ''
+      case '=':
+        return ' '
+    }
+    return match
+  })
+  return result
+}
+
+export { parseData, getDiff, formatText }
