d540d7c20b049859ee645887cfcc3bfa0f03fd8e
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-11 17:43:08 +0300

    изменен скрипт нахождения пути к файлу, утилиту можно запустить из любой папки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
-import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { parseData, getDiff } from '../index.js'
 
 const program = new Command()
@@ -14,11 +14,13 @@ program
   .version('0.0.1')
   .option('-f, --format', 'output format')
   .action((filepath1, filepath2) => {
-    const fullPathFile1 = path.resolve(process.cwd(), filepath1)
-    const fullPathFile2 = path.resolve(process.cwd(), filepath2)
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename)
+    const fullPathFile1 = path.resolve(process.cwd(), __dirname, '..', filepath1)
+    const fullPathFile2 = path.resolve(process.cwd(), __dirname, '..', filepath2)
     const parsedFile1 = parseData(fullPathFile1)
     const parsedFile2 = parseData(fullPathFile2)
-    return getDiff(parsedFile1, parsedFile2)
+    console.log(getDiff(parsedFile1, parsedFile2))
   })
 // добавить возможность передавать аргументы и вызывать тут функцию получения абсолютного пути
 program.parse()
