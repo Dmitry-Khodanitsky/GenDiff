1c685425666bd399e0e8d96c91abe024365e5bdc
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-14 18:13:51 +0300

    добавлено приведение передаваемого пути к нижнему регистру

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -16,10 +16,23 @@ program
   .action((filepath1, filepath2) => {
     const __filename = fileURLToPath(import.meta.url)
     const __dirname = path.dirname(__filename)
-    const fullPathFile1 = path.resolve(process.cwd(), __dirname, '..', filepath1)
-    const fullPathFile2 = path.resolve(process.cwd(), __dirname, '..', filepath2)
-    const parsedFile1 = parseData(fullPathFile1)
-    const parsedFile2 = parseData(fullPathFile2)
+
+    const fullPathFile1 = path.resolve(
+      process.cwd(),
+      __dirname,
+      '..',
+      filepath1,
+    )
+    const fullPathFile2 = path.resolve(
+      process.cwd(),
+      __dirname,
+      '..',
+      filepath2,
+    )
+
+    const parsedFile1 = parseData(fullPathFile1.toLowerCase())
+    const parsedFile2 = parseData(fullPathFile2.toLowerCase())
+
     console.log(getDiff(parsedFile1, parsedFile2))
   })
 // добавить возможность передавать аргументы и вызывать тут функцию получения абсолютного пути
