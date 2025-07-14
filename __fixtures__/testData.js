c1d4ea46cbe713758595de475178c2a7d36f8fb8
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-14 18:05:50 +0300

    добавлены тестовые данные yml

--- __fixtures__/testData.js
+++ __fixtures__/testData.js
@@ -1,18 +1,18 @@
-const jsonData = {
+const objData = {
   host: 'hexlet.io',
   timeout: 50,
   proxy: '123.234.53.22',
   follow: false,
 }
 
-const jsonData2 = {
+const objData2 = {
   timeout: 20,
   verbose: true,
   host: 'hexlet.io',
 }
 
 const diffString
-= '{"- follow":false,"= host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}'
+  = '{"- follow":false,"= host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}'
 
 const diffFormatString = `{ 
 - follow: false
@@ -23,4 +23,4 @@ const diffFormatString = `{
 + verbose: true
 }`
 
-export { jsonData, jsonData2, diffString, diffFormatString }
+export { objData, objData2, diffString, diffFormatString }
