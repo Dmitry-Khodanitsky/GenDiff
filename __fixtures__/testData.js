996b5eba09f7ae38edf231b74fef3136d5b6b0ce
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-16 15:09:52 +0300

    тесты разделены по разным файлам

--- __fixtures__/testData.js
+++ __fixtures__/testData.js
@@ -1,26 +1,172 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const getFixturePath = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+
 const objData = {
-  host: 'hexlet.io',
-  timeout: 50,
-  proxy: '123.234.53.22',
-  follow: false,
+  common: {
+    setting1: 'Value 1',
+    setting2: 200,
+    setting3: true,
+    setting6: {
+      key: 'value',
+      doge: {
+        wow: '',
+      },
+    },
+  },
+  group1: {
+    baz: 'bas',
+    foo: 'bar',
+    nest: {
+      key: 'value',
+    },
+  },
+  group2: {
+    abc: 12345,
+    deep: {
+      id: 45,
+    },
+  },
 }
-
 const objData2 = {
-  timeout: 20,
-  verbose: true,
-  host: 'hexlet.io',
+  common: {
+    follow: false,
+    setting1: 'Value 1',
+    setting3: null,
+    setting4: 'blah blah',
+    setting5: {
+      key5: 'value5',
+    },
+    setting6: {
+      key: 'value',
+      ops: 'vops',
+      doge: {
+        wow: 'so much',
+      },
+    },
+  },
+  group1: {
+    foo: 'bar',
+    baz: 'bars',
+    nest: 'str',
+  },
+  group3: {
+    deep: {
+      id: {
+        number: 45,
+      },
+    },
+    fee: 100500,
+  },
 }
 
-const diffString
-  = '{"- follow":false,"= host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}'
+const buildedTree = [
+  {
+    type: 'unchanged',
+    name: 'common',
+    children: [
+      { type: 'added', name: 'follow', newValue: false, children: [] },
+      {
+        type: 'unchanged',
+        name: 'setting1',
+        value: 'Value 1',
+        children: [],
+      },
+      {
+        type: 'removed',
+        name: 'setting2',
+        oldValue: 200,
+        children: [],
+      },
+      {
+        type: 'changed',
+        name: 'setting3',
+        oldValue: true,
+        newValue: null,
+        children: [],
+      },
+      {
+        type: 'added',
+        name: 'setting4',
+        newValue: 'blah blah',
+        children: [],
+      },
+      {
+        type: 'added',
+        name: 'setting5',
+        newValue: { key5: 'value5' },
+        children: [],
+      },
+      {
+        type: 'unchanged',
+        name: 'setting6',
+        children: [
+          {
+            type: 'unchanged',
+            name: 'doge',
+            children: [
+              {
+                type: 'changed',
+                name: 'wow',
+                oldValue: '',
+                newValue: 'so much',
+                children: [],
+              },
+            ],
+          },
+          {
+            type: 'unchanged',
+            name: 'key',
+            value: 'value',
+            children: [],
+          },
+          {
+            type: 'added',
+            name: 'ops',
+            newValue: 'vops',
+            children: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: 'unchanged',
+    name: 'group1',
+    children: [
+      {
+        type: 'changed',
+        name: 'baz',
+        oldValue: 'bas',
+        newValue: 'bars',
+        children: [],
+      },
+      { type: 'unchanged', name: 'foo', value: 'bar', children: [] },
+      {
+        type: 'changed',
+        name: 'nest',
+        oldValue: { key: 'value' },
+        newValue: 'str',
+        children: [],
+      },
+    ],
+  },
+  {
+    type: 'removed',
+    name: 'group2',
+    oldValue: { abc: 12345, deep: { id: 45 } },
+    children: [],
+  },
+  {
+    type: 'added',
+    name: 'group3',
+    newValue: { deep: { id: { number: 45 } }, fee: 100500 },
+    children: [],
+  },
+]
 
-const diffFormatString = `{ 
-- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true
-}`
-
-export { objData, objData2, diffString, diffFormatString }
+export { objData, objData2, getFixturePath, buildedTree }
--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,46 +1,15 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { expect, test } from '@jest/globals'
-import { getDiff, formatText } from '../src/lib.js'
-import { parseData } from '../src/parse.js'
+import { getDiff, buildTree } from '../src/lib.js'
+
 import {
+  buildedTree,
   objData,
   objData2,
-  diffString,
-  diffFormatString,
 } from '../__fixtures__/testData.js'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const getFixturePath = filename =>
-  path.join(__dirname, '..', '__fixtures__', filename)
-
-/* const readFixtureFile = filename =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8')
-*/
-
-test('File parse test JSON', () => {
-  const testFilePath = getFixturePath('file1.json')
-  const expected = objData
-  expect(parseData(testFilePath)).toEqual(expected)
-})
-
-test('Parse YML file', () => {
-  const testFilePath = getFixturePath('file1.yml')
-  expect(parseData(testFilePath)).toEqual(objData)
-})
-
-test('Parse YAML file', () => {
-  const testFilePath = getFixturePath('file2.yaml')
-  expect(parseData(testFilePath)).toEqual(objData2)
-})
-
-test('Formating string by formatText function', () => {
-  const expected = diffFormatString
-  expect(formatText(diffString)).toEqual(expected)
-})
-
-test('Get differences test', () => {
-  const expected = diffFormatString
-  expect(getDiff(objData, objData2)).toEqual(expected)
+test('Building tree from objects', () => {
+  const expected = buildedTree
+  expect(buildTree(objData, objData2)).toEqual(expected)
 })
