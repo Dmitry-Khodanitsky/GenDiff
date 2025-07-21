6a4314e5a84f7bb78b342174e8ca93eefe8ce305
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-21 17:58:04 +0300

    добавлен импорт json форматтера

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,13 +1,15 @@
 import { stylish } from './stylish.js'
 import { plain } from './plain.js'
-
+import { convertToJson } from './json.js'
 export default function (tree, format) {
   switch (format) {
     case 'stylish':
       return stylish(tree)
     case 'plain':
       return plain(tree)
+    case 'json':
+      return convertToJson(tree)
     default:
-      throw new Error('Неверный формат, формат может быть plain или stylish')
+      throw new Error('Неверный формат, доступные форматы plain, stylish, json')
   }
 }
