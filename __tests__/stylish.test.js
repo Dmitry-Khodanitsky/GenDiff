1a0e4bf89279b78e7a64cb4831dbcc6c9af7102c
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-21 12:46:14 +0300

    изменен путь импорта форматтера

--- __tests__/stylish.test.js
+++ __tests__/stylish.test.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
 import { buildedTree } from '../__fixtures__/testData.js'
-import { stylish } from '../src/stylish'
-import { asTree } from '../__fixtures__/formatAsTree'
+import { stylish } from '../src/formatters/stylish.js'
+import { asTree } from '../__fixtures__/formatAsTree.js'
 
 test('Test formatting as tree', () => {
   const expected = asTree
