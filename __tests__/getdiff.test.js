c48030401873d01a23f15127666242c58c58c65b
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-21 09:51:20 +0300

    изменено название теста с lib на getdiff

---
+++ __tests__/getdiff.test.js
@@ -0,0 +1,11 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { expect, test } from '@jest/globals'
+import { getDiff, buildTree } from '../src/getdiff.js'
+
+import { buildedTree, objData, objData2 } from '../__fixtures__/testData.js'
+
+test('Building tree from objects', () => {
+  const expected = buildedTree
+  expect(buildTree(objData, objData2)).toEqual(expected)
+})
