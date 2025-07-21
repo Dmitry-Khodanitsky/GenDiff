af86abef1f92990fbc51d50f67a809d281f15f9d
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-21 17:58:33 +0300

    добавлен тест форматера json

---
+++ __tests__/json.test.js
@@ -0,0 +1,19 @@
+import { expect, test } from '@jest/globals'
+import { buildedTree } from '../__fixtures__/testData.js'
+import { convertToJson } from '../src/formatters/json.js'
+import { __dirname  } from '../__fixtures__/testData.js'
+import fs from 'fs'
+import path from 'path'
+
+// Читаем фикстуру
+
+test('Test formatting as tree', () => {
+  const expected = fs
+    .readFileSync(
+      path.join(__dirname, 'jsonFormat.json'),
+      'utf-8'
+    )
+    .trim()
+
+  expect(convertToJson(buildedTree)).toBe(expected)
+})
