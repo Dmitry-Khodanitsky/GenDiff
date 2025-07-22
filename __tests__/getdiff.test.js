5f00cd11b93d6c4f641cbd25d7cc678812b90e69
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-22 20:21:35 +0300

    обновлен тест основной функциональности

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -1,9 +1,25 @@
 import { expect, test } from '@jest/globals'
-import { buildTree } from '../src/getdiff.js'
+import path from 'path'
+import { plainResult } from '../__fixtures__/expectedResultPlain'
+import { stylishResult } from '../__fixtures__/expectedResultStylish'
+import { jsonResult } from '../__fixtures__/expectedResultJSON'
+import genDiff from '../src/genDiff'
 
-import { buildedTree, objData, objData2 } from '../__fixtures__/testData.js'
+const testData = [
+  [
+    '__fixtures__/file1.json',
+    '__fixtures__/file2.json',
+    'stylish',
+    stylishResult,
+  ],
+  ['__fixtures__/file1.json', '__fixtures__/file2.yml', 'plain', plainResult],
+  ['__fixtures__/file1.YAML', '__fixtures__/file2.yml', 'stylish', stylishResult],
+  ['__fixtures__/file1.json', '__fixtures__/file2.YAML', 'json', jsonResult],
 
-test('Building tree from objects', () => {
-  const expected = buildedTree
-  expect(buildTree(objData, objData2)).toEqual(expected)
+]
+
+describe.each(testData)('Позитивный тест функции genDiff', (pathfile1, pathfile2, format, expected) => {
+  test(`Формат вывода ${format} для файлов ${path.extname(pathfile1)} и ${path.extname(pathfile2)}`, () => {
+    expect(genDiff(pathfile1, pathfile2, format)).toBe(expected)
+  })
 })
