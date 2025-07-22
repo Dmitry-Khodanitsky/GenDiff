42b7f8275642e4fa07415a3779f4012109a7dd62
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-22 20:20:33 +0300

    исправления линтера

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,34 +2,35 @@ import _ from 'lodash'
 
 function plain(tree) {
   function iter(nodes, parentPath = '') {
-    const result = nodes
-      .flatMap((node) => {
-        const { name, oldValue, newValue, type, children = [] } = node
-        const currentPath = parentPath ? `${parentPath}.${name}` : name
+    const result = nodes.flatMap((node) => {
+      const { name, oldValue, newValue, type, children = [] } = node
+      const currentPath = parentPath ? `${parentPath}.${name}` : name
 
-        const stringify = (value) => {
-          if (value === null) return 'null'
-          if (typeof value === 'number') return value
-          if (typeof value === 'boolean') return value.toString()
-          if (_.isObject(value) && !Array.isArray(value)) return '[complex value]'
-          return `'${value}'`
-        }
+      const stringify = (value) => {
+        if (value === null) return 'null'
+        if (typeof value === 'number') return value
+        if (typeof value === 'boolean') return value.toString()
+        if (_.isObject(value) && !Array.isArray(value)) return '[complex value]'
+        return `'${value}'`
+      }
 
-        switch (type) {
-          case 'added':
-            return `Property '${currentPath}' was added with value: ${stringify(newValue)}`
-          case 'removed':
-            return `Property '${currentPath}' was removed`
-          case 'changed':
-            return `Property '${currentPath}' was updated. From ${stringify(oldValue)} to ${stringify(newValue)}`
-          case 'nested':
-            return iter(children, currentPath)
-          case 'unchanged':
-            return []
-          default:
-            return []
-        }
-      })
+      switch (type) {
+        case 'added':
+          return `Property '${currentPath}' was added with value: ${stringify(
+            newValue,
+          )}`
+        case 'removed':
+          return `Property '${currentPath}' was removed`
+        case 'changed':
+          return `Property '${currentPath}' was updated. From ${stringify(
+            oldValue,
+          )} to ${stringify(newValue)}`
+        case 'nested':
+          return iter(children, currentPath)
+        default:
+          return []
+      }
+    })
 
     return result.filter(Boolean)
   }
