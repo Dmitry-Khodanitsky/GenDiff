52e63927156e0ce8d90d0ac8a3892b537b02e7f3
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-22 20:51:51 +0300

    изменен путь файлов в тестах

--- __tests__/getdiff.test.js
+++ __tests__/getdiff.test.js
@@ -1,25 +1,32 @@
 import { expect, test } from '@jest/globals'
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { plainResult } from '../__fixtures__/expectedResultPlain'
 import { stylishResult } from '../__fixtures__/expectedResultStylish'
 import { jsonResult } from '../__fixtures__/expectedResultJSON'
 import genDiff from '../src/genDiff'
 
-const testData = [
-  [
-    '__fixtures__/file1.json',
-    '__fixtures__/file2.json',
-    'stylish',
-    stylishResult,
-  ],
-  ['__fixtures__/file1.json', '__fixtures__/file2.yml', 'plain', plainResult],
-  ['__fixtures__/file1.YAML', '__fixtures__/file2.yml', 'stylish', stylishResult],
-  ['__fixtures__/file1.json', '__fixtures__/file2.YAML', 'json', jsonResult],
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const getFixturePath = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
 
+const testData = [
+  ['file1.json', 'file2.json', 'stylish', stylishResult],
+  ['file1.json', 'file2.yml', 'plain', plainResult],
+  ['file1.YAML', 'file2.yml', 'stylish', stylishResult],
+  ['file1.json', 'file2.YAML', 'json', jsonResult],
 ]
 
-describe.each(testData)('Позитивный тест функции genDiff', (pathfile1, pathfile2, format, expected) => {
-  test(`Формат вывода ${format} для файлов ${path.extname(pathfile1)} и ${path.extname(pathfile2)}`, () => {
-    expect(genDiff(pathfile1, pathfile2, format)).toBe(expected)
-  })
-})
+describe.each(testData)(
+  'Позитивный тест функции genDiff',
+  (pathfile1, pathfile2, format, expected) => {
+    test(`Формат вывода ${format} для файлов ${path.extname(
+      pathfile1,
+    )} и ${path.extname(pathfile2)}`, () => {
+      expect(
+        genDiff(getFixturePath(pathfile1), getFixturePath(pathfile2), format),
+      ).toEqual(expected)
+    })
+  },
+)
