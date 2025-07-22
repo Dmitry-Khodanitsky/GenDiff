82da1b8fecb0b722baa03629be4a81d2332f2a71
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-22 20:25:59 +0300

    удалены лишние тесты

--- __tests__/plain.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { buildedTree } from '../__fixtures__/testData.js'
-import { plain } from '../src/formatters/plain.js'
-import { plainFormat } from '../__fixtures__/expectedResultPlain.js'
-
-test('Test formatting as plain', () => {
-  const expected = plainFormat
-  expect(plain(buildedTree)).toEqual(expected)
-})
--- __tests__/stylish.test.js
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { buildedTree } from '../__fixtures__/testData.js'
-import { stylish } from '../src/formatters/stylish.js'
-import { asTree } from '../__fixtures__/expectedResultStylish.js'
-
-test('Test formatting as stylish', () => {
-  const expected = asTree
-  expect(stylish(buildedTree)).toEqual(expected)
-})
