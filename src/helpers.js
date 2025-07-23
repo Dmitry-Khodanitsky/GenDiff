a4c0e6828c160291ae58dc6db5a0d1d8a079f9b5
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-07-23 15:02:17 +0300

    изменена логика построения пути и чтения файла

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,14 @@
 import path from 'path'
 import fs from 'fs'
+import { fileURLToPath } from 'url'
 
-const readFile = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath)
-  const data = fs.readFileSync(fullPath).toString()
-  return data
-}
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const getPath = filename =>
+  path.resolve(__dirname, '..', '__fixtures__', filename)
+
+const readFile = filename => fs.readFileSync(getPath(filename), 'utf-8')
 
 const getExtension = (filepath) => {
   return path.extname(filepath.toLowerCase()).slice(1)
